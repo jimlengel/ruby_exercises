7bb1d2a8b4a3427a32246483ddc54b364c1cb491
JackMarx <seriousfools@gmail.com>
2017-01-09 20:48:15 -0600

    attr reader and writer

--- employee.rb
+++ employee.rb
@@ -11,31 +11,14 @@
 # puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes #{employee_2[:salary]} a year."
 
 class Employee
+  attr_reader :first_name, :last_name, :salary, :active
+  attr_writer :active
+
   def initialize(input_first_name, input_last_name, input_salary, input_active)
       @first_name = input_first_name
       @last_name = input_last_name
       @salary = input_salary
       @active = input_active
-  end
-
-  def first_name
-    @first_name
-  end
-
-  def last_name
-    @last_name
-  end
-
-  def salary
-    @salary
-  end
-
-  def active=(new_input_active)
-    @active = new_input_active
-  end
-
-  def active
-    @active
   end
 
   def print_info
@@ -55,7 +38,7 @@ employee_1.print_info
 # employee_1.give_annual_raise
 # employee_1.print_info
 
-puts employee_2.first_name.upcase
+puts employee_2.first_name
 puts employee_2.last_name
 puts employee_2.salary
 puts employee_2.active
